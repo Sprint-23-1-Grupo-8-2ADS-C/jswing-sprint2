bd0bf8be47ff6213cbf1776526dbfcfc0de574cc
Marcus Gonçalves <marcus.goncalves@sptech.school>
2023-04-13 18:01:47 -0300

    Fix conexão Azure SQL Server

--- jswing-sprint2/src/main/java/sptech/jswing/sprint2/Models.java
+++ jswing-sprint2/src/main/java/sptech/jswing/sprint2/Models.java
@@ -1,11 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package sptech.jswing.sprint2;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -19,18 +16,19 @@ public class Models {
     private String sqlDatabase;
     private String sqlUser;
     private String sqlPassword;
-    
-    
+    private String connectURL;
+
     public Models() {
         this.servername = "airplane-solutions.database.windows.net";
         this.sqlInstanceName = "airplane-solutions";
         this.sqlDatabase = "bd-airplane-solutions";
-        this.sqlUser = "totemInfo@airplane-solutions";
+        this.sqlUser = "totemInfo";
         this.sqlPassword = "Totem_!nf0";
+        this.connectURL = "jdbc:sqlserver://" + servername + ":1433;"
+            + "database=" + sqlDatabase + ";user=" + sqlUser  + "@" + sqlInstanceName + ";password=" + sqlPassword + ";"
+            + "encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
     }
-     
-   
-    private String connectURL;
+    
     
     public void testarConexao() {
         try(Connection conn = DriverManager.getConnection(connectURL); Statement stmt = conn.createStatement();) {
@@ -40,4 +38,20 @@ public class Models {
             e.printStackTrace();
         }
     }
+    
+    public void selectTotem() {
+        try(Connection conn = DriverManager.getConnection(connectURL); Statement stmt = conn.createStatement();) {
+            String resultadoConexao = "Conectado no Banco da Azure";
+            String query = "SELECT t.idTotem, f.senha FROM Totem as t "
+                    + "INNER JOIN companhiaAerea as ca ON ca.idCompanhia = t.fkCompanhia "
+                    + "INNER JOIN funcionario as f ON f.fkCompanhia = ca.idCompanhia;";
+            ResultSet rs = stmt.executeQuery(query);
+            System.out.println(resultadoConexao);
+            System.out.println(rs);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    
 }
