f2c971ad921e2d23b1cbccca4cdb571e81ed123f
giovanna-franca <giovanna.franca@hpe.com>
2023-05-17 20:30:49 -0300

    JAR enviando mensagem pro slack

---
+++ jswing-sprint2/src/main/java/sptech/jswing/sprint2/controllers/SlackIntegration.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sptech.jswing.sprint2.controllers;
+
+import com.github.seratch.jslack.Slack;
+import com.github.seratch.jslack.api.methods.SlackApiRequest;
+import com.github.seratch.jslack.api.webhook.Payload;
+import com.github.seratch.jslack.api.webhook.WebhookResponse;
+import java.net.http.HttpClient;
+
+/**
+ *
+ * @author franca
+ */
+public class SlackIntegration {
+
+    private static String webHooksUrl = "https://hooks.slack.com/services/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";
+    private static String oAuthToken = "xoxb-5282339203682-5282252399603-Ptyh7RjIvZPDNIg37Yg0EZG9";
+    private static String slackChannel = "airplanesolutions";
+//    private static HttpClient client = HttpClient.newHttpClient();
+
+    public static void main(String[] args) {
+        sendMessageToSlack("Primeira mensagem de teste da automação Slack");
+    }
+
+    public static void sendMessageToSlack(String message) {
+
+        try {
+            
+            StringBuilder msgBuilder = new StringBuilder();
+            
+            msgBuilder.append(message);
+
+            Payload payload = Payload.builder().channel(slackChannel).text(msgBuilder.toString()).build();
+
+            WebhookResponse wbResp = Slack.getInstance().send(webHooksUrl, payload);
+                    
+
+        }catch(Exception e){
+            e.printStackTrace();
+            
+        }
+
+    }
+}
