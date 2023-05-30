1bd2cfd31615af3289ba28a4ed35aa189cb522a6
giovanna-franca <giovanna.franca@hpe.com>
2023-05-30 20:03:07 -0300

    integração com o Slack

--- jswing-sprint2/src/main/java/sptech/jswing/sprint2/controllers/SlackIntegration.java
+++ jswing-sprint2/src/main/java/sptech/jswing/sprint2/controllers/SlackIntegration.java
@@ -1,25 +1,17 @@
 package sptech.jswing.sprint2.controllers;
 
 import com.github.seratch.jslack.Slack;
-import com.github.seratch.jslack.api.methods.SlackApiRequest;
 import com.github.seratch.jslack.api.webhook.Payload;
 import com.github.seratch.jslack.api.webhook.WebhookResponse;
-import java.net.http.HttpClient;
 
 public class SlackIntegration {
 
-    private static String webHooksUrl = "https://hooks.slack.com/services/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";
-    private static String oAuthToken = "xoxb-5282339203682-5282252399603-Ptyh7RjIvZPDNIg37Yg0EZG9";
-    private static String slackChannel = "airplanesolutions";
-    //private static HttpClient client = HttpClient.newHttpClient();
+    private static final String webHooksUrl = "https://hooks.slack.com/services/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";
+    private static final String oAuthToken = "xoxb-5282339203682-5282252399603-omwYsDyHSR4ZO5Zqir51YCBc";
+    private static final String slackChannel = "airplanesolutions";
 
-    /*
-    public static void main(String[] args) {
-        sendMessageToSlack("Primeira mensagem de teste da automação Slack");
-    }
-    */
 
-    public static void sendMessageToSlack(String message) {
+    public void sendMessageToSlack(String message) {
 
         try {
             
@@ -29,7 +21,8 @@ public class SlackIntegration {
 
             Payload payload = Payload.builder().channel(slackChannel).text(msgBuilder.toString()).build();
 
-            WebhookResponse wbResp = Slack.getInstance().send(webHooksUrl, payload);
+            WebhookResponse wbResp;
+            wbResp = Slack.getInstance().send(webHooksUrl, payload);
                     
 
         }catch(Exception e){
